a6c37917f08389f70caa2cc6ec13e0089af73eff
Thales Brasileiro <thalesxbrasileiro@gmail.com>
2022-05-09 22:29:04 -0300

    Conclusão do backend

---
+++ backend/controllers/UserController.js
@@ -0,0 +1,47 @@
+import User from "../models/UserModel.js";
+
+export const getUsers = async (req, res) => {
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+}
+
+export const getUserById = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        res.json(user);
+    } catch (error) {
+        res.status(404).json({message: error.message});
+    }
+}
+
+export const saveUser = async (req, res) => {
+    const user = new User(req.body);
+    try {
+        const insertedUser = await user.save();
+        res.status(201).json(insertedUser);
+    } catch (error) {
+        res.status(400).json({message: error.message});
+    }
+}
+
+export const updateUser = async (req, res) => {
+    try {
+        const updatedUser = await User.updateOne({_id:req.params.id}, {$set: req.body});
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        res.status(400).json({message: error.message});
+    }
+}
+
+export const deleteUser = async (req, res) => {
+    try {
+        const deleteUser = await User.deleteOne({_id:req.params.id});
+        res.status(200).json(deleteUser);
+    } catch (error) {
+        res.status(400).json({message: error.message});
+    }
+}
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import UserRoute from "./routes/UserRoute.js";
 
 const app = express();
 mongoose.connect('mongodb://localhost:27017/fullstack_db',{
@@ -13,5 +14,6 @@ db.once('open', () => console.log('Banco de dados conectado...'));
 
 app.use(cors());
 app.use(express.json());
+app.use(UserRoute);
 
 app.listen(5000, () => console.log('O servidor está no ar...'));
